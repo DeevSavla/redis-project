860d06f97e87d46a21634aac7f018af363e95649
Deev Savla <savla.deev@gmail.com>
2025-08-06 09:20:01 +0530

    4: redis setup

--- routes/restaurants.ts
+++ routes/restaurants.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import { validate } from "../middlewares/validate.js";
 import { restaurantSchema, type Restaurant } from "../schemas/restaurant.js";
+import { initialiseRedisClient } from "../utils/client.js";
 const router = express.Router();
 
 router.post("/", validate(restaurantSchema), async (req, res) => {
   const data = req.body as Restaurant;
+  const client = await initialiseRedisClient();
   res.send("Hello World");
 });
 
--- schemas/reviews.ts
+++ schemas/reviews.ts
@@ -1,8 +1,8 @@
-import {z} from 'zod'
+import { z } from "zod";
 
 export const reviewSchema = z.object({
-    review: z.string().min(1),
-    rating: z.number().min(1).max(5)
-})
+  review: z.string().min(1),
+  rating: z.number().min(1).max(5),
+});
 
-export type Review = z.infer<typeof reviewSchema>
+export type Review = z.infer<typeof reviewSchema>;
