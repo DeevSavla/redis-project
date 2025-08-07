667898fa97f2bf17d0ea96bf035178f200e6b700
Deev Savla <savla.deev@gmail.com>
2025-08-07 11:34:38 +0530

    6: Lists

--- routes/restaurants.ts
+++ routes/restaurants.ts
@@ -3,9 +3,14 @@ import { validate } from "../middlewares/validate.js";
 import { restaurantSchema, type Restaurant } from "../schemas/restaurant.js";
 import { initialiseRedisClient } from "../utils/client.js";
 import { nanoid } from "nanoid";
-import { restaurantKeyById } from "../utils/keys.js";
-import { successResponse } from "../utils/responses.js";
+import {
+  restaurantKeyById,
+  reviewKeyById,
+  reviewDetailsKeyById,
+} from "../utils/keys.js";
+import { errorResponse, successResponse } from "../utils/responses.js";
 import { checkRestaurantExists } from "../middlewares/checkRestaurantId.js";
+import { reviewSchema, type Review } from "../schemas/reviews.js";
 const router = express.Router();
 
 router.post("/", validate(restaurantSchema), async (req, res, next) => {
@@ -27,6 +32,92 @@ router.post("/", validate(restaurantSchema), async (req, res, next) => {
     next(error);
   }
 });
+
+router.post(
+  "/:restaurantId/reviews",
+  checkRestaurantExists,
+  validate(reviewSchema),
+  async (req: Request<{ restaurantId: string }>, res, next) => {
+    const { restaurantId } = req.params;
+    const data = req.body as Review;
+    try {
+      const client = await initialiseRedisClient();
+      const reviewId = nanoid();
+      const reviewKey = reviewKeyById(restaurantId);
+      const reviewDetailsKey = reviewDetailsKeyById(reviewId);
+      const reviewData = {
+        id: reviewId,
+        ...data,
+        timestamp: Date.now(),
+        restaurantId,
+      };
+      await Promise.all([
+        client.lPush(reviewKey, reviewId),
+        client.hSet(reviewDetailsKey, reviewData),
+      ]);
+      return successResponse(res, reviewData, "Review added");
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.get(
+  "/:restaurantId/reviews",
+  checkRestaurantExists,
+  async (req: Request<{ restaurantId: string }>, res, next) => {
+    const { restaurantId } = req.params;
+    //this brings the top limit reviews for that restaurant, here limit = 1 means brings latest one review only.
+    const { page = 1, limit = 1 } = req.query;
+    const start = (Number(page) - 1) * Number(limit);
+    const end = start + Number(limit) - 1;
+    try {
+    } catch (error) {}
+    try {
+      const client = await initialiseRedisClient();
+      const reviewKey = reviewKeyById(restaurantId);
+      const reviewIds = await client.lRange(reviewKey, start, end);
+      const reviews = await Promise.all(
+        reviewIds.map((id) => client.hGetAll(reviewDetailsKeyById(id)))
+      );
+      return successResponse(
+        res,
+        reviews,
+        `Fetched all reviews for ${restaurantId}`
+      );
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.delete(
+  "/:restaurantId/reviews/reviewId",
+  checkRestaurantExists,
+  async (
+    req: Request<{ restaurantId: string; reviewId: string }>,
+    res,
+    next
+  ) => {
+    const { restaurantId, reviewId } = req.body;
+    try {
+      const client = await initialiseRedisClient();
+      const reviewKey = reviewKeyById(restaurantId);
+      const reviewDetailsKey = reviewDetailsKeyById(reviewId);
+      const [removeResult, deleteResult] = await Promise.all([
+        //go to reviewkey and remove all reviews with that id
+        client.lRem(reviewKey, 0, reviewId),
+        client.del(reviewDetailsKey),
+      ]);
+      if (removeResult == 0 && deleteResult == 0) {
+        return errorResponse(res, 404, "Review not found");
+      }
+      return successResponse(res, reviewId, `Review ${reviewId} deleted.`);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
 router.get(
   "/:restaurantId",
--- utils/keys.ts
+++ utils/keys.ts
@@ -6,6 +6,5 @@ export const restaurantKeyById = (id:string) => getKeyName("restaurants",id);
 
 export const reviewKeyById = (id:string) => getKeyName("reviews",id);
 
-export const reviewDetailsKeyById = (id:string) => getKeyName("reviews",id);
-
+export const reviewDetailsKeyById = (id:string) => getKeyName("review_details",id);
 
